d0c4934c984ae79521584cdf60e28d77e85efc32
wilypomegranate <wilypomegranate@users.noreply.github.com>
2019-02-13 22:27:38 -0500

    Disabled thread-safe statics for the avr platform.
    Those symbols aren't implemented nor relevant here.

--- toolchain/avr/avr.cmake
+++ toolchain/avr/avr.cmake
@@ -11,5 +11,5 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 # TODO Make this platform independent.
 include_directories(SYSTEM /usr/avr/include)
 
-set(CMAKE_C_FLAGS "-Os -Werror -Wall -pedantic -Wextra")
-set(CMAKE_CXX_FLAGS "-Os -Werror -Wall -pedantic -Wextra")
+set(CMAKE_C_FLAGS "-Os -Werror -Wall -pedantic -Wextra -fno-threadsafe-statics")
+set(CMAKE_CXX_FLAGS "-Os -Werror -Wall -pedantic -Wextra -fno-threadsafe-statics")
