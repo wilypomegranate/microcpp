851742d00e245edc399ec6bbbc62bcc05f41c64b
wilypomegranate <wilypomegranate@users.noreply.github.com>
2019-02-13 21:59:22 -0500

    Removed unused code.

--- tests/device_tests/set_register/test.cpp
+++ tests/device_tests/set_register/test.cpp
@@ -9,15 +9,10 @@ using namespace microcpp;
 int main() {
 
   Console console(9600);
-  // console.write(TCCR0A);
-  // console.write("\n");
   Register test(TCCR0A);
   test.set_bits(WGM00, WGM01);
 
   console.fmt("WGM00: {}\tWGM01: {}\tTCCR0A: {}\n", WGM00, WGM01, TCCR0A);
-  // console.write("{}\n");
 
-  // console.write(TCCR0A);
-  // console.write("\n");
   return 0;
 }
